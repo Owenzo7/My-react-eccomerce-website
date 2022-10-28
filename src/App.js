10aa221b9f49ee383ad5e0df8b9963d90c9ffa10
Owen Lee <owenlee545@gmail.com>
2022-10-28 15:58:26 +0300

    Implemented react router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import Discounted from "./components/Discounted";
-import Explore from "./components/Explore";
-import Featured from "./components/Featured";
+
 import Footer from "./components/Footer";
-import Highlights from "./components/Highlights";
-import Landing from "./components/Landing";
 import Nav from "./components/Nav";
+import {BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from "./pages/Home";
 
 
 
 
 function App() {
   return (
+
+    <Router>
     <div className="App">
       <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
+      <Route path="/" exact component={Home}/>
       <Footer />
     </div>
+    </Router>
   );
 }
 
