4cf6a22919e0cb44d11b7a6447781b699ecec4e8
Owen Lee <owenlee545@gmail.com>
2022-10-30 07:46:23 +0300

    Done with recommended books

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,19 +1,76 @@
-import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom";
+import Price from "../components/ui/Price";
+import Rating from "../components/ui/Rating";
 
-function BookInfo({books}) {
+function BookInfo({ books }) {
   return (
     <div id="books__body">
-        <main id="books__main">
-            <div className="books__container">
-                <div className="row">
-                    <div className="book__selected--top">
-                        
-                    </div>
-                </div>
+      <main id="books__main">
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <Link to="/books" className="book__link">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <Link to="/books" className="book__link">
+                <h2 className="book__selected--title--top">Books</h2>
+              </Link>
             </div>
-        </main>
+            <div className="book__selected">
+              <figure className="book__selected--figure">
+                <img
+                  src="https://covers.openlibrary.org/b/id/8091016-L.jpg"
+                  alt=""
+                  className="book__selected--ig"
+                />
+              </figure>
+              <div className="book__selected--description">
+                <h2 className="book__selected--title">
+                  Crack the coding interview
+                </h2>
+                <Rating rating="4.5" />
+                <div className="book__selected--price">
+                  <Price originalPrice={50} salePrice={20} />
+                </div>
+                <div className="book__summary">
+                  <div className="book__summary--title">Summary</div>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Aspernatur sit dolore consectetur corrupti veniam quia
+                    voluptate, incidunt aut sapiente mollitia delectus
+                    praesentium repellendus voluptates minus a iure quo ea
+                    excepturi.
+                  </p>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Aspernatur sit dolore consectetur corrupti veniam quia
+                    voluptate, incidunt aut sapiente mollitia delectus
+                    praesentium repellendus voluptates minus a iure quo ea
+                    excepturi.
+                  </p>
+                </div>
+
+                <button className="btn">Add to cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="books__container">
+            <div className="row">
+                <div className="book__selected--top">
+                    <h2 className="book__selected--title--top">
+                        Recommended Books
+                    </h2>
+                </div>
+
+            </div>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 
-export default BookInfo
+export default BookInfo;
