4ab41389ed40677a3196d1706f52cbaf09003e6a
Owen Lee <owenlee545@gmail.com>
2022-10-30 06:22:28 +0300

    Done with setting up the rating

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import {BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from "./pages/Home";
+import Books from "./pages/Books";
+import {books} from './data'
 
 
 
@@ -14,6 +16,7 @@ function App() {
     <div className="App">
       <Nav />
       <Route path="/" exact component={Home}/>
+      <Route path="/books"  render={()=> <Books books={books}/>} />
       <Footer />
     </div>
     </Router>
