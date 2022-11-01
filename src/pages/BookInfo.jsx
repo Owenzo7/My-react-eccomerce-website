42debe767addacc6676f65cbb55382421417fa22
Owen Lee <owenlee545@gmail.com>
2022-11-01 14:50:31 +0300

    done with the selection of books using the id method

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,10 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import Book from "../components/ui/Book";
 import Price from "../components/ui/Price";
 import Rating from "../components/ui/Rating";
 
 function BookInfo({ books }) {
+  const { id } = useParams();
+
+  const book = books.find((book) => +book.id === +id);
+
+  console.log(book);
+
   return (
     <div id="books__body">
       <main id="books__main">
@@ -20,22 +27,19 @@ function BookInfo({ books }) {
             </div>
             <div className="book__selected">
               <figure className="book__selected--figure">
-                <img
-                  src="https://covers.openlibrary.org/b/id/8091016-L.jpg"
-                  alt=""
-                  className="book__selected--ig"
-                />
+                <img src={book.url} alt="" className="book__selected--img" />
               </figure>
               <div className="book__selected--description">
-                <h2 className="book__selected--title">
-                  Crack the coding interview
-                </h2>
-                <Rating rating="4.5" />
+                <h2 className="book__selected--title">{book.title}</h2>
+                <Rating rating={book.rating} />
                 <div className="book__selected--price">
-                  <Price originalPrice={50} salePrice={20} />
+                  <Price
+                    originalPrice={book.originalPrice}
+                    salePrice={book.salePrice}
+                  />
                 </div>
                 <div className="book__summary">
-                  <div className="book__summary--title">Summary</div>
+                  <h3 className="book__summary--title">Summary</h3>
                   <p className="book__summary--para">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                     Aspernatur sit dolore consectetur corrupti veniam quia
@@ -62,6 +66,15 @@ function BookInfo({ books }) {
           <div className="row">
             <div className="book__selected--top">
               <h2 className="book__selected--title--top">Recommended Books</h2>
+            </div>
+
+            <div className="books">
+
+            {
+              books.filter(book => book.rating === 5 && +book.id !== +id)
+              .slice(0,4)
+              .map(book => <Book book={book} key={book.id} />)
+            }
             </div>
           </div>
         </div>
