588eeda474f57ee94a80c5840ca7e00c40076f1e
Owen Lee <owenlee545@gmail.com>
2022-11-01 17:19:22 +0300

    changes have been made

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
     <App />
-  
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
